2931e5e6f562aa14bc7aa6084cba31cd3351303a
kkurkela <kyleakurkela@gmail.com>
2017-04-13 13:00:25 -0400

    Init commit
    1.) Add pipeline.m
    2.) Add biopack_import.m

---
+++ biopack_import.m
@@ -0,0 +1,5 @@
+function [] = biopack_import()
+% Custom function for importing the MICE biopac data into PsPM
+
+
+end
---
+++ pipeline.m
@@ -0,0 +1,49 @@
+%%% Welcome to the MICE PsychoPhys Pipeline %%%
+% Written by Kyle Kurkela, April 2017
+
+%%% Step 0: Study Parameters
+
+rootdir  = '';
+
+% Subjects structure, with the fields:
+%   .flag   = 1 if you want to specify the ids to run in the .ids field, 2
+%             if you want to use a regular expression to grab subjects ids
+%   .ids    = a 1 x n cell array of subject ids strings to run
+%   .regexp = a regular expression, used to grab subjects ids
+Subjects.flag   = 2;
+Subjects.ids    = {'s001'};
+Subjects.regexp = 's...';
+
+% A 1 x n cell array of the different tasks to analyze (e.g., encoding,
+% retrieval)
+Tasks         = {'enc', 'ret'};
+
+% Rounds structure, with the fields:
+%   .flag   = 1 if you want to specify the ids to run in the .ids field, 2
+%             if you want to use a regular expression to grab subjects ids
+%   .ids    = a 1 x n cell array of strings detailing the names of the
+%             rounds
+%   .regexp = a regular expression, used to grab individual task rounds
+Rounds.flag   = 2;
+Rounds.ids    = {'round01' 'round02' 'round03' 'round04' 'round05' 'round06'};
+Rounds.regexp = 'round..';
+
+% Check to see if spm is on the MATLAB search path; if so remove it
+% from the search path; then check to see if PsPM is on the search
+% path; throw and error if pspm is not on the path
+spmcheck = which('spm');
+if ~isempty(spmcheck)
+    [spmpath,~,~] = fileparts(spmcheck);
+    rmpath(genpath(spmpath));
+end
+
+pspmcheck = which('pspm');
+if isempty(pspmcheck)
+    error('Please add PsPM to the MATLAB search path')
+end
+
+%%% Step 1: Importing
+
+biopac_import();
+
+%%% Step 2:
