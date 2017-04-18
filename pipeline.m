db27f0ac1129b0d7b6d1b7b81673198c201f6a8b
kkurkela <kyleakurkela@gmail.com>
2017-04-18 12:44:00 -0400

    Pipeline update
    1.) Added verbosity
    2.) Updated comments
    3.) Added task 2 'visual inspecting'
    4.) Began task 3 'downsample'

--- pipeline.m
+++ pipeline.m
@@ -3,7 +3,9 @@
 
 %%
 %%% Study Parameters
+% Define study and computer specific parameters
 
+% Where is the MICE Biopac data on your computer?
 rootdir  = '/Volumes/memolab/MICE/MICE_fMRI/Data/';
 
 % Subjects structure, with the fields:
@@ -29,12 +31,18 @@ Rounds.flag   = 2;
 Rounds.ids    = {'round01' 'round02' 'round03' 'round04' 'round05' 'round06'};
 Rounds.regexp = 'round..';
 
+
+% Verbose. Do you want the pipeline to print text to the Command Window or
+% run silently?
+global verbose
+verbose = true;
+
 %%
 %%% Checking
-
 % Check to see if spm is on the MATLAB search path; if so remove it
 % from the search path; then check to see if PsPM is on the search
 % path; throw and error if pspm is not on the path
+
 spmcheck = which('spm');
 if ~isempty(spmcheck)
     [spmpath,~,~] = fileparts(spmcheck);
@@ -47,12 +55,36 @@ if isempty(pspmcheck)
 end
 
 %%
-%%% Step 1: Importing
+%%% Task 1: Importing
 
-filein = fullfile(rootdir, Subjects.ids{1}, 'BioPac', 'sub_s001_round01_ret.txt');
-fileout = fullfile(pwd, 'output.txt');
-
-biopac_import(filein, fileout);
+if verbose
+    fprintf('Task 1: Importing Data ...\n') 
+end
+filein  = fullfile(rootdir, Subjects.ids{1}, 'BioPac', 'sub_s001_round01_ret.txt');
+fileout = fullfile(pwd, 'output.txt'); % change to something with subject name, round, and task
+pspm_filename = biopac_import(filein, fileout);
 
 %%
-%%% Step 2:
+%%% Task 2: Visually inspect and reject
+
+if verbose
+    fprintf('\nTask 2: Visual Inspection...\n') 
+end
+pspm_inspect(pspm_filename);
+
+STR = input('Accept Participant? y/n: ', 's');
+if strcmp(STR, 'y') || isempty(STR)
+    
+elseif strcmp(STR, 'n')
+    %continue
+end
+
+%%
+%%% Task 3: 
+if verbose
+    fprintf('\nTask 3: Downsample...\n') 
+end
+pspm_downsample(pspm_filename);
+
+%%
+%%% Task 4:
