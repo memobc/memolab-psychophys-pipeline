50a2968c386ae45a5d0fce767b5fcf40238b3ade
kkurkela <kyleakurkela@gmail.com>
2017-04-21 15:20:43 -0400

    Changed where the model was saved
    estimate_pspsm_model now saves the model in the subject's analysis folder

--- modules/estimate_pspm_model.m
+++ modules/estimate_pspm_model.m
@@ -1,4 +1,4 @@
-function estimate_pspm_model(datafile, modelfile, Analysis)
+function estimate_pspm_model(datafile, modelfile, Analysis, currentSubjectID)
 % Function for specifying a GLM for modeling MICE's scr data
 
     % Verbosity (i.e., tell the user what the hell is going on)
@@ -8,10 +8,10 @@ function estimate_pspm_model(datafile, modelfile, Analysis)
     end
 
     % set the matlabbatch structure (see subfunction below)
-    matlabbatch = set_model_batch(datafile, modelfile, Analysis.name, Analysis.dir);
+    matlabbatch = set_model_batch(datafile, modelfile, Analysis.name, fullfile(Analysis.dir, currentSubjectID));
     
     % run this job through pspm's version of the SPM job manager
-    scr_jobman('run', matlabbatch)
+    scr_jobman('interactive', matlabbatch)
 
     function matlabbatch = set_model_batch(filename, modelfile, name, dir)
         
@@ -33,7 +33,7 @@ function estimate_pspm_model(datafile, modelfile, Analysis)
         matlabbatch{1}.pspm{1}.first_level{1}.scr{1}.glm_scr.filter.edit.highpass.enable.order = 1;
         matlabbatch{1}.pspm{1}.first_level{1}.scr{1}.glm_scr.filter.edit.down                  = 10;   % downsample to 10 Hz
         matlabbatch{1}.pspm{1}.first_level{1}.scr{1}.glm_scr.filter.edit.direction             = 'uni';
-        matlabbatch{1}.pspm{1}.first_level{1}.scr{1}.glm_scr.overwrite                         = false;
+        matlabbatch{1}.pspm{1}.first_level{1}.scr{1}.glm_scr.overwrite                         = true;
         
     end
 
