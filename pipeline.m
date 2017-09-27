e42ab3d5b16df216603a37e97b4998ee719429df
kakurk <kurkela@bc.edu>
2017-09-27 11:03:33 -0400

    pipeline revamp
    1. Added contrasts module
    2. Updated documentation throughout

--- pipeline.m
+++ pipeline.m
@@ -1,11 +1,17 @@
-%%% Welcome to the MICE PsychoPhys Pipeline %%%
-% Written by Kyle Kurkela, April 2017
+%==========================================================================
+%                   The MemoLab Psychophys Pipeline
+%==========================================================================
+% Welcome to the MemoLab's Psychophys Pipeline
+% Written by Kyle Kurkela, kyleakurkela@gmail.com
+% April-October 2017
 
 %%
-%%% Study Parameters
+%==========================================================================
+%                   Study Specific Parameters
+%==========================================================================
 % Define study and computer specific parameters
 
-% Where dooes the MICE Biopac data live on your computer?
+% Where does the data live on your computer?
 rootdir  = '/Volumes/memolab/MICE/MICE_fMRI/data/';
 
 % Subjects structure, with the fields:
@@ -19,15 +25,26 @@ rootdir  = '/Volumes/memolab/MICE/MICE_fMRI/data/';
 %             overwritten with the id's identified using the regular
 %             expression.
 %   .regexp = a regular expression, used to grab subjects ids dynamically
-Subjects.flag   = false;
-Subjects.ids    = {'sub-s001','sub-s003','sub-s004','sub-s005','sub-s006',...
-                   'sub-s007','sub-s008','sub-s009','sub-s010','sub-s011',...
-                   'sub-s012','sub-s013','sub-s014'}; % excludes subject sub-s002
-Subjects.regexp = '^sub-s0..';
+Subjects.flag   = true;
+Subjects.ids    = {'sub-s001','sub-s002','sub-s003','sub-s004','sub-s005',...
+                   'sub-s006','sub-s007','sub-s008','sub-s009','sub-s010',...
+                   'sub-s011','sub-s012','sub-s013','sub-s014','sub-s015',...
+                   'sub-s016','sub-s017','sub-s018','sub-s019','sub-s020',...
+                   'sub-s021','sub-s022','sub-s023','sub-s024','sub-s025',...
+                   'sub-s026','sub-s027','sub-s028','sub-s029','sub-s030'...
+                   'sub-s031'};
+Subjects.regexp = '^sub-s0(?!02|15|18|21|23|24|25|29|31)'; % excludes subjects 2,15,18,21,23,24,25,29,31
 
 % A 1 x n cell array of the different tasks to analyze (e.g., encoding,
-% item retrieval, emotion retrieval).
-Tasks         = {'enc'}; % ret
+% item retrieval, emotion retrieval). The scripts searches the functional
+% run file names for this identifier. For example:
+%
+%   /sub01_round01_enc.txt
+%   /sub01_round01_ret.txt
+%
+% The Task cell array should contain the identifier 'enc' for the Encoding
+% task.
+Tasks         = {'enc'};
 
 % Rounds structure, with the fields:
 %   .flag   = true if you want to grab round IDs dynamically using a 
@@ -43,8 +60,9 @@ Tasks         = {'enc'}; % ret
 %             IDs from the biophys filenames. Google regular expressions,
 %             see spm_select's filter option
 Rounds.flag   = true;
-Rounds.ids    = {'round01','round02','round03','round04'};
-Rounds.regexp = 'round..';
+Rounds.ids    = {'round01','round02','round03','round04','round05'...
+                 'round06'};
+Rounds.regexp = 'round0[^7]'; % grab all available runs EXCEPT round07
 
 % Analysis structure, with the fileds:
 %   .root = the root directory where this analysis will be saved
@@ -54,20 +72,25 @@ Rounds.regexp = 'round..';
 %   .dir  = the directory holding this analysis, hardcoded here as a
 %           subfolder within the root directory
 Analysis.root = [pwd filesep 'models'];
-Analysis.name = 'Emotional_vs_Neutral_Trials'; % WhiteNoise_vs_AllOther; Emotional_vs_Neutral_Trials; Valence_and_Noise
+Analysis.name = 'Valence_and_Noise'; % WhiteNoise_vs_AllOther; Emotional_vs_Neutral_Trials; Valence_and_Noise
 Analysis.dir  = fullfile(Analysis.root, Analysis.name);
 
 % Verbose. Do you want the pipeline to print text to the Command Window or
-% run silently? TRUE = gives user various periodic updates
+% run silently? TRUE = gives user various periodic updates. Defined as a
+% global variable so that it is available for all functions and modules in
+% the pipeline.
 global verbose
-verbose = true;
+verbose = false;
 
 % visualize. Visually inspect and reject the raw data? True = yes, false =
 % no.
 visualize = false;
 
 %%
-%%% Pipeline Prep
+%==========================================================================
+%                   Pipeline Preperation
+%==========================================================================
+% Pipeline Prep. Things that need to be done before pipeline can be run.
 
 % Check to see if PsPM is on the search path; throw and error if pspm is 
 % not on the path
@@ -106,7 +129,10 @@ if Subjects.flag
 end
 
 %%
-%%% Task: Importing
+%==========================================================================
+%                           Importing
+%==========================================================================
+% Import the raw SCR data into MATLAB
 
 for curSubj = Subjects.ids
     
@@ -154,16 +180,18 @@ for curSubj = Subjects.ids
                 fprintf('\nTask 1: Importing Data ...\n') 
             end
             
-            % Grab the biopac data txt file for this Subject/Round/Task using spm_select and a regular expression
+            % Grab the biopac data txt file for this Subject/Round/Task using kyles_spm_select and a regular expression
             filein  = kyles_spm_select('FPList', curSubjBiopacDir, ['.*' curRound{:} '.*' lower(curTask{:}) '\.txt$']);
             
-            % Create the output filename
+            % Create the output file directory
             if ~exist(fullfile(curSubjBiopacDir, 'processed'), 'dir')
                 mkdir(fullfile(curSubjBiopacDir, 'processed'))
             end
+            % Output file name
             fileout = fullfile(curSubjBiopacDir, 'processed', [curSubj{:} '_' curRound{:} '_' lower(curTask{:}) '.mat']);
             
-            % Import the biopac data for this Subject/Round/Task
+            % Import the biopac data for this Subject/Round/Task. Store it
+            % in a structure pspm_filename
             pspm_filename.(clean_curSubj{:}).(curTask{:}).(curRound{:}) = biopac_import(filein{:}, fileout);
             
         end
@@ -171,8 +199,13 @@ for curSubj = Subjects.ids
 end
 
 %%
-%%% Task: Visually inspect and reject
+%==========================================================================
+%                  Visually inspect and reject
+%==========================================================================
+% Visually inspect and reject bad runs of data for each Subject/Task/Round,
+% if desired.
 
+% only perform is desired
 if visualize
     
     for curSubj = Subjects.ids
@@ -220,12 +253,15 @@ if visualize
 end
 
 %%
-%%% Task: Specify Model
+%==========================================================================
+%                           Specify Model
+%==========================================================================
+% Specify Model for each subject/task
 
 for curSubj = Subjects.ids
     
     % current subject ID without the BIDS 'sub-'
-    clean_curSubj = regexprep(curSubj, 'sub-', '');    
+    clean_curSubj = regexprep(curSubj, 'sub-', '');
     
     for curTask = Tasks
         
@@ -233,13 +269,16 @@ for curSubj = Subjects.ids
             fprintf('\nTask 5: Specifying Model...\n')
         end
         behav_data_dir = fullfile(rootdir, curSubj{:}, curTask{:});
-        modelfiles.(clean_curSubj{:}).(curTask{:}) = pspm_specify(behav_data_dir, curSubj{:}, Analysis);
+        modelfiles.(clean_curSubj{:}).(curTask{:}) = pspm_specify(behav_data_dir, curSubj{:}, Analysis, Rounds.regexp);
         
     end
 end
 
 %%
-%%% Task: Estimate Model
+%==========================================================================
+%                         Estimate Model
+%==========================================================================
+% Estimate Model for each subject/task
 
 for curSubj = Subjects.ids
     
@@ -271,6 +310,30 @@ for curSubj = Subjects.ids
             pspm_estimate_model(datafiles, multicondfiles, Analysis, curSubj{:}, curType{:});
             
         end
+        
+    end
+end
+
+%%
+%==========================================================================
+%                         Define Contrasts
+%==========================================================================
+% Define contrasts for each subject/model
+
+% for each subject...
+for curSubj = Subjects.ids
+    
+    % current subject ID without the BIDS 'sub-'
+    clean_curSubj = regexprep(curSubj, 'sub-', '');
+    
+    % for each task..
+    for curTask = Tasks
+        
+        if verbose
+            fprintf('\nTask 7: Running Contrasts...\n')
+        end
+        
+        pspm_constrasts(modelfiles.(clean_curSubj{:}).(curTask{:}));
         
     end
 end
